cc8c449581ab2cfbe3c3d1165c3dda8b99c955fc
Muqeetullah <87570384+Muqeetullah@users.noreply.github.com>
2023-09-20 17:13:53 +0500

    auth file

--- auth.ts
+++ auth.ts
@@ -1,48 +1,30 @@
 import NextAuth, {NextAuthConfig} from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import {SignInCredentials} from "./types/SigninCredential";
-
 const authConfig: NextAuthConfig = {
+  secret: process.env.AUTH_SECRET,
   providers: [
     CredentialsProvider({
-      credentials: {
-        // Define your expected credentials here
-        email: {label: "Email", type: "text"},
-        password: {label: "Password", type: "password"},
-      },
-      authorize: async (credentials, request) => {
+      id: "credentials",
+      name: "credentials",
+      type: "credentials",
+      credentials: {},
+      async authorize(credentials, request) {
         const {email, password} = credentials as SignInCredentials;
-        console.log(email, password);
-
-        try {
-          const response = await fetch(
-            "http://localhost:3000/api/users/signin",
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({email, password}),
-            }
-          );
-
-          const data = await response.json();
-          console.log("WEeeeeeeeeeeeeeeee herrrrrreeeeeeeeeeeeee");
-
-          if (data.error) {
-            throw new Error(data.error);
+        const {user, error} = await fetch(
+          "http://localhost:3000/api/users/signin",
+          {
+            method: "POST",
+            body: JSON.stringify({email, password}),
           }
+        ).then(async res => await res.json());
 
-          return {id: data.user.id};
-        } catch (error: any) {
-          console.error(error.message);
-          throw new Error("Authentication failed");
-        }
+        if (error) throw new Error(error);
+        return {id: user.id};
       },
     }),
   ],
 };
-
 export const {
   auth,
   handlers: {GET, POST},
